a85bb1685096b57e13de4088645b2d68c85c03cb
st-mn <56998558+st-mn@users.noreply.github.com>
2025-10-16 04:15:45 +0000

    startup.sh

--- startup.sh
+++ startup.sh
@@ -24,11 +24,13 @@ pip install celery && \
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb && \
 sudo apt-get install -y /tmp/google-chrome-stable_current_amd64.deb && \
 sudo apt-get install -y libnss3 libatk-bridge2.0-0t64 libgtk-3-0t64 libx11-xcb1 libxcb-dri3-0 libxcomposite1 libxdamage1 libxrandr2 libgbm1 libasound2t64 libpangocairo-1.0-0 libcups2t64 && \
-python3 api.py && \
+# python3 api.py & \
 sudo apt-get update && \
 sudo apt-get install -y xdg-utils && \
 curl -fsSL https://ollama.com/install.sh | sh && \
 export OLLAMA_HOST=0.0.0.0:11434 && \
-ollama serve && \
-./start_services.sh full && \
+ollama serve & \
+sleep 5 && \
+./start_services.sh full & \
+sleep 3 && \
 xdg-open http://localhost:3000
